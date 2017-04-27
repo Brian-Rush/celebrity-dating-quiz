c3a0cc2c20cf2ac558b3a7c8a922969293a376a6
Brian Rush and Jared Eiseman <jared@>
2017-04-27 13:26:23 -0700

    add bootstrap form validation styling and logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,29 @@ $(document).ready(function() {
     //evaluate field input. figure out logic (which combos go where)
     //show the appropriate div based on evaluation
 
-  $("#celeb-form").submit(function() {
-    $()
+  $("#celeb-form").submit(function(e) {
+    e.preventDefault();
+
+    if (!$("#wiggle").val()) {
+      $('#helpBlock1').show();
+      $('#wiggle').parent().addClass('has-error');
+    } else {
+      $('#helpBlock1').hide();
+      $('#wiggle').parent().removeClass('has-error');
+    }
+    if (!$('#urkel-false').prop('checked')) {
+      $('#helpBlock2').show();
+      $('#urkel-div').addClass('has-error');
+    } else {
+      $('#helpBlock2').hide();
+      $('#urkel-div').removeClass('has-error');
+    }
+    if (!$("#marsupial").val()) {
+      $('#helpBlock3').show();
+      $('#marsupial').parent().addClass('has-error');
+    } else {
+      $('#helpBlock3').hide();
+      $('#marsupial').parent().removeClass('has-error');
+    }
   });
 });
