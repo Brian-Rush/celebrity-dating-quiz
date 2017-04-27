e1afc3868f1dd37f51f133f42b665f2740bed2b5
Brian Rush and Jared Eiseman <brian@>
2017-04-27 12:00:54 -0700

    add urkel functionality, begin form validation styles

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("input#urkel-true").click(function() {
+    window.location = "img/urkel.jpg";
+  });
+
+  //form submit handler
+    //Do form validation (if field blank gives inline message).
+    //evaluate field input. figure out logic (which combos go where)
+    //show the appropriate div based on evaluation
+
+  $("#celeb-form").submit(function() {
+    $()
+  });
+});
